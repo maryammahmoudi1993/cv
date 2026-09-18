44531cd5b4c9793421c911551ccff1b417dfa4f3
maryammahmoudi1993 <mahmoodi.maryam1993@gmail.com>
2026-09-18 16:37:12 +0800

    feat: add admin login page and admin layout with sidebar navigation

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,7 +1,12 @@
 import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
 import App from './App.jsx';
 import '../styles.css';
 
 export function render() {
-  return renderToString(<App />);
+  return renderToString(
+    <StaticRouter basename="/cv" location="/">
+      <App />
+    </StaticRouter>,
+  );
 }
